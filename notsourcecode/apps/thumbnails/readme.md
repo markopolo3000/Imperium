8e23351d760c0c7d22db38642592e63bd8eafc58
Bigfoot9999 <bigfoot@officialbigfoot.tk>
2022-07-24 19:19:53 -0600

    Pre Release
    Stop stealing our code and write some yourself

---
+++ notsourcecode/apps/thumbnails/readme.md
@@ -0,0 +1,1 @@
+bigfoot was here :)
---
+++ notsourcecode/scripts/lock.js
@@ -0,0 +1,13 @@
+
+  var key;
+  var password="password";
+  key = prompt("Please enter the password");
+  if (key == password){
+
+  } 
+  else {
+  alert('You got the password wrong!');
+  window.location = "https://google.com";
+}
+
+
